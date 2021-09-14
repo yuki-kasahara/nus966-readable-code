e5eb2d7fe602c005cc01f0180990a1ed606f09ea
yamaji <crindigo0713@gmail.com>
2021-09-14 13:31:24 +0900

    spec4: Change script reading some words

--- OutputDictionary.py
+++ OutputDictionary.py
@@ -1,3 +1,5 @@
 
-with open("DictionaryData.txt","r",encoding="utf-8") as f:
-    print(f.read())
+with open("DictionaryData.txt","r",encoding="utf-8") as dictionary_data:
+    dictinary_list = dictionary_data.readlines()
+    for word in dictinary_list:
+        print(word, end='')
--- README.md
+++ README.md
@@ -8,3 +8,7 @@
 仕様３
 - "上手"という単語をtextファイル（DictionaryData.txt）に保存。
 - OutputDictinary.pyでopen関数を使ってデータファイルの呼び出し。
+
+仕様４
+- 辞書データファイル（DictionaryData.txt）に改行で単語の追加
+- OutputDictinary.pyで全単語の出力
